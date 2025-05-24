b61f0aad316e0422ef0632bcc561cbeea74b38a5
Heet <heet16@gmail.com>
2025-05-24 22:53:12 +0530

    Delete JS/supabaseClient.js

--- JS/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-
-const supabaseUrl = '__SUPABASE_URL__';
-const supabaseKey = '__SUPABASE_KEY__';
-
-window.supabase = createClient(supabaseUrl, supabaseKey);
